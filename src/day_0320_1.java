bbf9711b0ad2f12ed68912742c5617c2d9a59038
zhanxuzhao <649912323@qq.com>
2019-03-20 17:59:04 +0800

    stack queue

--- src/day_0320_1.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class day_0320_1 {
-
-    @Test
-    public void test() {
-        System.out.println(123);
-    }
-}
---
+++ src/day_0320_stack_queue.java
@@ -0,0 +1,84 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class day_0320_stack_queue {
+
+    @Test
+    public void testMyStack() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+
+    @Test
+    public void testMyQueue() {
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+    }
+
+    class MyStack<E> {
+        private Queue<E> mQ1 = new LinkedList<>();
+        private Queue<E> mQ2 = new LinkedList<>();
+
+        private Queue<E> getQueueFrom() {
+            return !mQ1.isEmpty() ? mQ1 : mQ2;
+        }
+
+        private Queue<E> getQueueTo() {
+            return mQ1.isEmpty() ? mQ1 : mQ2;
+        }
+
+        public void push(E data) {
+            getQueueFrom().add(data);
+        }
+
+        public E pop() {
+            E last = null;
+            Queue<E> qFrom = getQueueFrom();
+            Queue<E> qTO = getQueueTo();
+            while (!qFrom.isEmpty()) {
+                E poll = qFrom.poll();
+                if (!qFrom.isEmpty()) {
+                    qTO.add(poll);
+                } else {
+                    last = poll;
+                }
+            }
+            return last;
+        }
+
+        public E peek() {
+            return null;
+        }
+    }
+
+    class MyQueue<E> {
+        private Stack<E> mStack1 = new Stack<>();
+        private Stack<E> mStack2 = new Stack<>();
+
+        public void push(E data) {
+            mStack1.add(data);
+        }
+
+        public E pop() {
+            if (mStack2.isEmpty()) {
+                while (!mStack1.isEmpty()) {
+                    mStack2.push(mStack1.pop());
+                }
+            }
+            return mStack2.pop();
+        }
+    }
+}
